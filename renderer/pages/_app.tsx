0ad610b69628519279b2a62b815b7ff06f660f82
m19e <nexus.me7290@gmail.com>
2021-04-01 21:30:59 +0900

    Format

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -1,16 +1,16 @@
-import type { AppProps } from 'next/app'
-import '../styles/index.css'
-import Head from 'next/head'
+import type { AppProps } from "next/app";
+import "../styles/index.css";
+import Head from "next/head";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return (
-    <>
-      <Head>
-        <title>Home - Nextron (with-typescript-tailwindcss)</title>
-      </Head>
-      <Component {...pageProps} />
-    </>
-  )
-}
+    return (
+        <>
+            <Head>
+                <title>Home - Nextron (with-typescript-tailwindcss)</title>
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
+};
 
-export default MyApp
+export default MyApp;
